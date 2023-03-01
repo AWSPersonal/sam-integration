7fd171fb3669aaa66b4feaa5f3cf7b647532f923
Manjunath PV <manjunath.poilath@psiog.com>
2023-03-01 19:24:33 +0530

    Integrated sns

---
+++ hello_world/app.py
@@ -0,0 +1,14 @@
+import json
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function"""
+    print(event)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
