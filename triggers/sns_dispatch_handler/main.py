455d0a43b1e9f8091540b71c8a6351d921c85bab
Manjunath PV <manjunath.poilath@psiog.com>
2023-03-03 11:53:41 +0530

    mods on role for state-machine

--- triggers/sns_dispatch_handler/main.py
+++ triggers/sns_dispatch_handler/main.py
@@ -2,14 +2,21 @@ import boto3
 import json
 import os
 
-def handler(event,context):
+
+def handler(event, context):
     """Sample pure Lambda function"""
     state_machine_arn = os.environ['STATE_MACHINE_ARN']
+    access_key = os.environ['ACCESS_KEY']
+    secret_key = os.environ['SECRET_KEY']
     input_data = {
         'key1': 'value1',
         'key2': 'value2'
     }
-    client = boto3.client('stepfunctions')
+    print("The ARN is ", state_machine_arn)
+    client = boto3.client(
+        'stepfunctions',
+        aws_access_key_id=f"{access_key}",
+        aws_secret_access_key=f"{secret_key}",)
     sns_messages = []
     records = event["Records"]
     if records:
@@ -17,7 +24,7 @@ def handler(event,context):
             print(record["Sns"]["Message"])
             sns_messages.append(record["Sns"]["Message"])
 
-    response = client.start_execution(
+    client.start_execution(
         stateMachineArn=state_machine_arn,
         input=json.dumps(input_data)
     )
